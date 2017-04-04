79be16b62ee93d801fe99f08d9194fff791f6cb0
Mohamed Amine Ouali <M.AMine.Wali@gmail.com>
2017-04-04 18:40:42 +0100

    Integration_Comptes_Employees_addPrimes

--- Human_Resources/Client/Scripts/APP/app.js
+++ Human_Resources/Client/Scripts/APP/app.js
@@ -10,6 +10,8 @@ var myApp = angular.module('myApp', [
   'myApp.controllers',
   'myApp.ComptesControllor',
   'myApp.ComptesFactories',
+  'myApp.EmployeesControllor',
+  'myApp.EmployeesFactory',
   'myApp.PrimsContollor',
   'myApp.PrimsFactories',
   'myApp.factories',
@@ -20,6 +22,7 @@ var myApp = angular.module('myApp', [
     $routeProvider.when('/', { templateUrl: 'public/views/Home.html', controller: 'MainControllor' });
     $routeProvider.when('/login', { templateUrl: 'public/views/Login.html', controller: 'LoginControllor' });
     $routeProvider.when('/comptes', { templateUrl: 'public/views/Comptes/Comptes.html', controller: 'Main.Comptes' });
+    $routeProvider.when('/employees', { templateUrl: 'public/views/Employes/Employes.html', controller: 'Main.Employees' });
     $routeProvider.when('/AddPrim', { templateUrl: 'public/views/Prims/addPrim.html', controller: 'Add.Prim' });
     $routeProvider.otherwise({ redirectTo: '/login' });
 }]);
