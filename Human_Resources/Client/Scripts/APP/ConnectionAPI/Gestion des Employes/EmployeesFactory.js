6895f7b642a8ebe40d1647081d9fca5581e01ea2
Mohamed Amine Ouali <M.AMine.Wali@gmail.com>
2017-04-06 00:22:32 +0100

    UploadFile To server

--- Human_Resources/Client/Scripts/APP/ConnectionAPI/Gestion des Employes/EmployeesFactory.js
+++ Human_Resources/Client/Scripts/APP/ConnectionAPI/Gestion des Employes/EmployeesFactory.js
@@ -3,13 +3,41 @@
 /* Directives */
 
 
-angular.module('myApp.EmployeesFactory', []).
-factory('Employees', ['$http', '$q', 'ServiceURL', 'userService', '$httpParamSerializer', function ($http, $q, serviceBasePath, userService, $httpParamSerializer) {
-    var fac = {};
-    fac.GetAllEmployees = function () {
-        return $http.get(serviceBasePath + '/api/Employee/GetAll').then(function (response) {
-            return response.data;
-        })
-    }
-    return fac;
+angular.module('myApp.EmployeesFactory', [])
+    .factory('Employees', ['$http', '$q', 'ServiceURL', 'userService', '$httpParamSerializer', function ($http, $q, serviceBasePath, userService, $httpParamSerializer) {
+        var fac = {};
+        fac.GetAllEmployees = function () {
+            return $http.get(serviceBasePath + '/api/Employee/GetAll').then(function (response) {
+                return response.data;
+            })
+        }
+
+        fac.AddEmployees = function (data) {
+            var obj = {
+                "filesize": data.filesize,
+                "filetype": data.filetype,
+                "filename": data.filename,
+                "base64Data": data.base64
+            };
+
+
+            var defer = $q.defer();
+
+            $http({
+                method: 'POST',
+                url: serviceBasePath + "/api/Employee/UploadFile",
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                data: JSON.stringify(obj) 
+            }).then(function (response) {
+                defer.resolve(response.data);
+
+            }, function (error) {
+                defer.reject(error.data);
+            })
+            return defer.promise;
+        }
+
+        return fac;
 }]);
--- Human_Resources/Client/Scripts/APP/app.js
+++ Human_Resources/Client/Scripts/APP/app.js
@@ -15,7 +15,8 @@ var myApp = angular.module('myApp', [
   'myApp.PrimsContollor',
   'myApp.PrimsFactories',
   'myApp.factories',
-  'myApp.interceptor'
+  'myApp.interceptor',
+  'naif.base64'
 ]).config(['$locationProvider', function($locationProvider) {
     $locationProvider.hashPrefix('');
 }]).config(['$routeProvider', function ($routeProvider) {
@@ -23,6 +24,7 @@ var myApp = angular.module('myApp', [
     $routeProvider.when('/login', { templateUrl: 'public/views/Login.html', controller: 'LoginControllor' });
     $routeProvider.when('/comptes', { templateUrl: 'public/views/Comptes/Comptes.html', controller: 'Main.Comptes' });
     $routeProvider.when('/employees', { templateUrl: 'public/views/Employes/Employes.html', controller: 'Main.Employees' });
+    $routeProvider.when('/uploadEmployees', { templateUrl: 'public/views/Employes/UploadEmployees.html', controller: 'Upload.Employees' });
     $routeProvider.when('/AddPrim', { templateUrl: 'public/views/Prims/addPrim.html', controller: 'Add.Prim' });
     $routeProvider.otherwise({ redirectTo: '/login' });
 }]);
