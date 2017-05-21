62f2d0255f322bdc9b800e1fd06f2ba42be72469
Mohamed Amine Ouali <M.AMine.Wali@gmail.com>
2017-05-21 23:38:17 +0100

    Interface fiche de paie

---
+++ Human_Resources/Human_Resources/Metier/Traitement/Salaire/FicheDEPAieGenerator.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Human_Resources.Metier.Model;
+
+namespace Human_Resources.Metier.Traitement.Salaire
+{
+    public class FicheDePaieGenerator : IFicheDePaieGeneretor
+    {
+        public string GetDateDebut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDateFin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employe GetEmploye()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal HeureSupplementaire()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal HeureSupplementaireMontant()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal heureSupplementaireTaux()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal MontantCNAMPatrenale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal MontantCNAMSalariale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal MontantCNRPSPatrenale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal MontantCNRPSSalariale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal MontantCNSSPatrenale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal MontantCNSSSalariale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal MontantIGR()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal Prime()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal SalaireBase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal SalaireBaseHeure()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal SalaireBrute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal SalaireNet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TauxCNAMPatrenale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TauxCNAMSalairale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TauxCNRPSPatrenale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TauxCNRPSSalairale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TauxCNSSPatrenale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TauxCNSSSalairale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TauxHoraireSalaireBase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TauxIGR()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Human_Resources/Human_Resources/Metier/Traitement/Salaire/IFicheDePaieGeneretor.cs
@@ -0,0 +1,47 @@
+﻿
+
+using Human_Resources.Metier.Model;
+
+namespace Human_Resources.Metier.Traitement.Salaire
+{
+    interface IFicheDePaieGeneretor
+    {
+        string GetDateDebut();
+        string GetDateFin();
+        Employe GetEmploye();
+
+        decimal SalaireBaseHeure();
+        decimal TauxHoraireSalaireBase();
+        decimal SalaireBase();
+        decimal HeureSupplementaire();
+        decimal heureSupplementaireTaux();
+        decimal HeureSupplementaireMontant();
+        decimal Prime();
+        decimal SalaireBrute();
+
+        decimal TauxCNSSSalairale();
+        decimal MontantCNSSSalariale();
+        decimal TauxCNSSPatrenale();
+        decimal MontantCNSSPatrenale();
+
+
+        decimal TauxCNRPSSalairale();
+        decimal MontantCNRPSSalariale();
+        decimal TauxCNRPSPatrenale();
+        decimal MontantCNRPSPatrenale();
+
+        decimal TauxCNAMSalairale();
+        decimal MontantCNAMSalariale();
+        decimal TauxCNAMPatrenale();
+        decimal MontantCNAMPatrenale();
+
+
+        decimal TauxIGR();
+        decimal MontantIGR();
+
+
+        decimal SalaireNet();
+
+
+    }
+}
